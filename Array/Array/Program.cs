68bbecb5d385018084fdc4ec71b47c047ac70a84
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-04 21:58:31 +0530

    [ADDED]: JAYESH code for input Array elements and print them in Asp.Net

---
+++ Array/Array/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[5];
+            int i;
+            Console.WriteLine("Please Enter the 5 Element");
+            for (i = 0; i < 5; i++)
+            {
+                a[i] = Convert.ToInt16(Console.ReadLine());
+
+            }
+            Console.WriteLine("The Array Elements Are:");
+            for (i = 0; i < 5; i++)
+            {
+                Console.WriteLine(a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
