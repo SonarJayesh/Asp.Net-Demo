c3a6b25b97b359903b81441241c19a4d625d40e1
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-06 15:59:16 +0530

    [ADDED]: JAYESH code for Find Factorial of given Number Using do_While loop in Asp.Net

---
+++ Do_While_Factorial/Do_While_Factorial/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Do_While_Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Find factorial of Given Number");
+
+            int num,fact=1;
+
+            System.Console.WriteLine("Enter The Number");
+            num = Convert.ToInt16(System.Console.ReadLine());
+
+            while (num != 0) {
+
+                fact = fact * num;
+
+                num--;
+            }
+            System.Console.WriteLine("Factorial is:" + fact);
+            System.Console.ReadLine();
+
+        }
+    }
+}
