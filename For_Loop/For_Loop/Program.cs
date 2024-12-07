f234e9383d50c5282dc6faa34a61e79696210f2d
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-07 21:44:57 +0530

    [ADDED]: JAYESH iterate the numbers using the for loop in Asp.Net

---
+++ For_Loop/For_Loop/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace For_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c = 0, i, level = 0;
+
+            System.Console.WriteLine("Enter the Level Number");
+            level = Convert.ToInt16(System.Console.ReadLine());
+
+            System.Console.WriteLine(a +"\n"+b);
+
+            for (i = 1; i <= level; i++)
+            {
+                c = a + b;
+                System.Console.WriteLine(c);
+
+                a = b;
+                b = c;
+            
+            }
+                System.Console.ReadLine();
+        }
+    }
+}
