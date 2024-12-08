e22c3a68e41f3ae7954e8bf53d3dcb53c48a4c2f
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-08 11:04:53 +0530

    [ADDED]: JAYESH code for if_else condition to find the Number is Even Or Odd in Asp.Net

---
+++ If_else_EvenOdd/If_else_EvenOdd/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace If_else_EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0;
+
+            System.Console.WriteLine("Enter the No:");
+            n = Convert.ToInt16(System.Console.ReadLine());
+
+            if (n % 2 == 0)
+            {
+
+                System.Console.WriteLine(n + " Is Even \n");
+            }
+            else
+            {
+
+                System.Console.WriteLine(n + " Is Odd");
+            }
+
+            System.Console.ReadLine();
+
+        }
+    }
+}
