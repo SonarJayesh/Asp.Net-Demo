4bdb0cec2f0b323cf656791ef7aeff3ed8c74202
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-08 11:03:57 +0530

    [ADDED]: JAYESH code for if_else_Ladder in Asp.Net

---
+++ If_Else_Ladder/If_Else_Ladder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace If_Else_Ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            System.Console.WriteLine("Enter the Number:");
+
+            a = Convert.ToInt16(System.Console.ReadLine());
+
+            if (a == 10)
+            {
+                System.Console.WriteLine("Number is a 10");
+            }
+            else if (a == 20)
+            {
+                System.Console.WriteLine("Number Is a 20");
+            }
+            else if (a == 30)
+            {
+                System.Console.WriteLine("Number is a 30");
+            }
+            else
+            {
+                System.Console.WriteLine("Number is Not a Match");
+            }
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
