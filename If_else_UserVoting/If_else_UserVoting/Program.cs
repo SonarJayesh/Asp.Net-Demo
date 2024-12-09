e7697c64f0253180a2333859a207716d1c27c650
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-09 18:42:45 +0530

    [ADDED]: JAYESH Asp.Net code for Simple if else statement program to User Voting

---
+++ If_else_UserVoting/If_else_UserVoting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace If_else_UserVoting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age=0;
+            System.Console.WriteLine("User input Age and check Allowed For Voting or Not ");
+            
+            System.Console.WriteLine("Enter The Age");
+            
+            age = Convert.ToInt16(System.Console.ReadLine());
+
+            if (age >= 18){
+
+                System.Console.WriteLine("Allowed for Voting");
+            }
+            else{
+
+                System.Console.WriteLine("Not Allowed for Voting");
+            }
+
+            System.Console.ReadLine();
+        }
+    }
+}
