15b507b9f94d30c5028f25497652a4c614e65833
SonarJayesh <jayeshsonar2824@gmail.com>
2024-12-10 20:42:53 +0530

    [ADDED]: JAYESH Asp.Net code for simple nested_if else Demo code

---
+++ Nested_If_Else/Nested_If_Else/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nested_If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 0, c = 0;
+            System.Console.WriteLine("Enter The 3 Number:");
+            a = Convert.ToInt16( System.Console.ReadLine());
+            b = Convert.ToInt16(System.Console.ReadLine());
+            c = Convert.ToInt16(System.Console.ReadLine());
+
+            if (a > b)
+            {
+                if (a > c)
+                {
+                    System.Console.WriteLine(a + " Is a Big");
+                }
+                else
+                {
+                    System.Console.WriteLine(c + " Is a Big");
+                }
+            }
+
+            else
+            {
+                if (b > c)
+                {
+                    System.Console.WriteLine(b + " Is a Big");
+                }
+                else 
+                {
+                    System.Console.WriteLine(c + " Is a Big");
+                }
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
